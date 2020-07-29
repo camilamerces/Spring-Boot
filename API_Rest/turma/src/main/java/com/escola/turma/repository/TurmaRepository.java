0853a019b4b1ca34b42f11f21b1f0ab563828fb9
camilamerces <camilamerces1@gmail.com>
2020-07-29 10:03:29 -0300

    CRUD Back-end turma

---
+++ API_Rest/turma/src/main/java/com/escola/turma/repository/TurmaRepository.java
@@ -0,0 +1,15 @@
+package com.escola.turma.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.escola.turma.model.Turma;
+
+@Repository
+public interface TurmaRepository extends JpaRepository<Turma, Long> {
+	
+	public List<Turma> findAllByTurmaContainingIgnoreCase (String turma);
+
+}
